8c89a63c2b4f7d5629a39415cf2c8353c97e6ddc
Lunix-420 <davidhess336@gmx.de>
2024-08-09 20:44:18 +0200

    Implement static seed for ShapercurveComponent

--- Source/ShapercurveComponent.cpp
+++ Source/ShapercurveComponent.cpp
@@ -1,129 +1,121 @@
 #include "ShapercurveComponent.h"
 
-
-ShapercurveComponent::ShapercurveComponent(PlasmaAudioProcessor& p, int stage) : audioProcessor(p)
+ShapercurveComponent::ShapercurveComponent(PlasmaAudioProcessor& p, int stage)
+  : audioProcessor(p)
 {
-	//Listen to parameter changes
-	const auto& params = audioProcessor.getParameters();
-	for (auto param : params)
-	{
-		param->addListener(this);
-	}
-	startTimerHz(60);
-	this->stage = stage;
+  // Listen to parameter changes
+  const auto& params = audioProcessor.getParameters();
+  for (auto param : params) {
+    param->addListener(this);
+  }
+  startTimerHz(60);
+  this->stage = stage;
 }
 
 ShapercurveComponent::~ShapercurveComponent()
 {
-	const auto& params = audioProcessor.getParameters();
-	for (auto param : params)
-	{
-		param->removeListener(this);
-	}
+  const auto& params = audioProcessor.getParameters();
+  for (auto param : params) {
+    param->removeListener(this);
+  }
 }
 
-void ShapercurveComponent::parameterValueChanged(int parameterIndex, float newValue)
+void
+ShapercurveComponent::parameterValueChanged(int parameterIndex, float newValue)
 {
-	parametersChanged.set(true);
+  parametersChanged.set(true);
 }
 
-void ShapercurveComponent::timerCallback()
+void
+ShapercurveComponent::timerCallback()
 {
-	if (parametersChanged.compareAndSetBool(false, true))
-	{
-		repaint();
-	}
+  if (parametersChanged.compareAndSetBool(false, true)) {
+    repaint();
+  }
 }
 
-void ShapercurveComponent::paint(juce::Graphics& g)
+void
+ShapercurveComponent::paint(juce::Graphics& g)
 {
-	using namespace juce;
+  using namespace juce;
 
-	//Get Chain Settings
-	auto chainSettings = getChainSettings(audioProcessor.apvts);
+  // Get Chain Settings
+  auto chainSettings = getChainSettings(audioProcessor.apvts);
 
-	//bounds
-	auto bounds = getBounds();
-	auto lineSize = bounds.getHeight() / 100;
-    lineSize = (lineSize < 1.0) ? 1.0f : lineSize;
+  // bounds
+  auto bounds = getBounds();
+  auto lineSize = bounds.getHeight() / 100;
+  lineSize = (lineSize < 1.0) ? 1.0f : lineSize;
 
-	//Screen
-	auto x = 0;
-	auto y = 0;
-	auto w = bounds.getWidth();
-	auto h = bounds.getHeight();
+  // Screen
+  auto x = 0;
+  auto y = 0;
+  auto w = bounds.getWidth();
+  auto h = bounds.getHeight();
 
-	//Info
-	float drive = 2.0;
-	Distortion type = Distortion::Hardclip;
-	float bias = 0.0;
-	float girth = 0.0;
-	switch (stage)
-	{
-	case 0:
-	{
-		type = chainSettings.driveType;
-		drive = chainSettings.drive;
-		bias = chainSettings.bias;
-		girth = chainSettings.girth;
-		break;
-	}
-	case 1:
-	{
-		type = chainSettings.lateDriveType;
-		drive = chainSettings.lateDrive;
-		bias = chainSettings.lateBias;
-		girth = chainSettings.lateGirth;
-		break;
-	}
-	}
-	float sampleSize = 2.0 / w;
+  // Info
+  float drive = 2.0;
+  Distortion type = Distortion::Hardclip;
+  float bias = 0.0;
+  float girth = 0.0;
+  switch (stage) {
+    case 0: {
+      type = chainSettings.driveType;
+      drive = chainSettings.drive;
+      bias = chainSettings.bias;
+      girth = chainSettings.girth;
+      break;
+    }
+    case 1: {
+      type = chainSettings.lateDriveType;
+      drive = chainSettings.lateDrive;
+      bias = chainSettings.lateBias;
+      girth = chainSettings.lateGirth;
+      break;
+    }
+  }
+  float sampleSize = 2.0 / w;
 
-	//Draw
-	std::vector<double> samples;
-	samples.resize(w);
-	for (int i = 0; i < w; ++i)
-	{
-		//Write
-		samples[i] = i * sampleSize - 1;
+  // Draw
+  std::vector<double> samples;
+  samples.resize(w);
+  juce::Random random(420);
+  for (int i = 0; i < w; ++i) {
+    // Write
+    samples[i] = i * sampleSize - 1;
 
-		//Distort
-		DistortionProcessor::distort(samples[i], drive, type);
+    // Distort
+    DistortionProcessor::distort(samples[i], drive, type);
 
-		//Girth
-		samples[i] = samples[i] *
-			((((float)(rand() % 100)) / 100 * abs(girth)) + 1);
+    // Girth
+    samples[i] = samples[i] * ((random.nextFloat() * abs(girth)) + 1);
 
-		//Bias
-		if (samples[i] > 0)
-		{
-			samples[i] += samples[i] * bias;
-		}
-		else if (samples[i] < 0)
-		{
-			samples[i] -= samples[i] * bias;
-		}
-	}
-	Path shaperCurve;
-	const double outputMin = y + h;
-	const double outputMax = y;
-	auto map = [outputMin, outputMax](double input)
-	{
-		return jmap(input, -1.0, 1.0, outputMin, outputMax);
-	};
+    // Bias
+    if (samples[i] > 0) {
+      samples[i] += samples[i] * bias;
+    } else if (samples[i] < 0) {
+      samples[i] -= samples[i] * bias;
+    }
+  }
+  Path shaperCurve;
+  const double outputMin = y + h;
+  const double outputMax = y;
+  auto map = [outputMin, outputMax](double input) {
+    return jmap(input, -1.0, 1.0, outputMin, outputMax);
+  };
 
-	shaperCurve.startNewSubPath(x, map(samples.front()));
-	for (size_t i = 1; i < samples.size(); ++i)
-	{
-		shaperCurve.lineTo(x + i, map(samples[i]));
-	}
-    
-    g.setColour(Colours::white);
-    g.drawRect(x, y, w, h, 2.0 * lineSize);
-    g.strokePath(shaperCurve, PathStrokeType(1.5 * lineSize));
+  shaperCurve.startNewSubPath(x, map(samples.front()));
+  for (size_t i = 1; i < samples.size(); ++i) {
+    shaperCurve.lineTo(x + i, map(samples[i]));
+  }
+
+  g.setColour(Colours::white);
+  g.drawRect(x, y, w, h, 2.0 * lineSize);
+  g.strokePath(shaperCurve, PathStrokeType(1.5 * lineSize));
 }
 
-void ShapercurveComponent::setColor(Colour c)
+void
+ShapercurveComponent::setColor(Colour c)
 {
-	color = c;
+  color = c;
 }
