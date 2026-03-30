8bd1049601778a6d1a3ceea6c3be61afddca0242
Lunix-420 <davidhess336@gmx.de>
2026-03-30 20:19:04 +0200

    fix: MacOs build stuff

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -155,7 +155,7 @@
  #define JucePlugin_ARAFactoryID           "com.Dimethoxy.Plasma.factory"
 #endif
 #ifndef  JucePlugin_ARADocumentArchiveID
- #define JucePlugin_ARADocumentArchiveID   "com.Dimethoxy.Plasma.aradocumentarchive.1.2.1"
+ #define JucePlugin_ARADocumentArchiveID   "com.Dimethoxy.Plasma.aradocumentarchive.1.3.0"
 #endif
 #ifndef  JucePlugin_ARACompatibleArchiveIDs
  #define JucePlugin_ARACompatibleArchiveIDs  ""
